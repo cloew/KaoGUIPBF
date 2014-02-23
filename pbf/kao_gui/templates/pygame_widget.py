80e21c75ef240a88b76befb4d3a417b1ab0c2d95
Chris Loew <cloew123@gmail.com>
2014-02-23 15:44:02 -0700

    Fixed typo in Pygame Widget template

--- pbf/kao_gui/templates/pygame_widget.py
+++ pbf/kao_gui/templates/pygame_widget.py
@@ -1,6 +1,6 @@
 from kao_gui.pygane.pygame_widget import PygameWidget
 
-class %WidgetName%(ConsoleWidget):
+class %WidgetName%(PygameWidget):
     """ Represents the widget for a *** """
     
     def __init__(self):
