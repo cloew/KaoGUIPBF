bf2f5028c696e35f3d20c93c5c4a6801687eae7c
Chris Loew <cloew123@gmail.com>
2014-03-01 11:57:40 -0700

    Added Command to make a pygame controller

--- pbf/kao_gui/Commands/new_pygame_controller.py
+++ pbf/kao_gui/Commands/new_pygame_controller.py
@@ -6,32 +6,32 @@ from pbf.python.helpers.python_helper import GetPythonImportString
 from pbf.templates import template_manager
 from pbf.kao_gui.templates import TemplatesRoot
 
-class NewConsoleController:
-    """ Creates a new Console Controller """
+class NewPygameController:
+    """ Command to Create a new Pygame Controller """
     category = "new"
-    command = "cns-ctrl"
-    description = "Creates a new Console Controller"
+    command = "pygame-ctrl"
+    description = "Creates a new Pygame Controller"
     minimumNumberOfArguments = 1
     
     def run(self, args):
-        """ ADD LOGIC TO RUN THE PACKAGE HERE """
+        """ Run the command """
         controllerFileName = args[0]
         controllerName = GetPythonClassnameFromFilename(controllerFileName)
-        print "Creating Console Controller:", controllerName, "at:", controllerFileName
+        print "Creating Pygame Controller:", controllerName, "at:", controllerFileName
         self.createController(controllerFileName, controllerName)
         
-    def createController(self, controllerFileName, controllerName, ):
+    def createController(self, controllerFileName, controllerName):
         """ Create the controller file """
         viewName = controllerName.replace("Controller", "Screen")
         viewFileName = controllerFileName.replace("controller", "screen")
         keywords = {"%ControllerName%":controllerName,
                     "%ViewName%":viewName,
                     "%ViewImport%":GetPythonImportString(viewFileName, [viewName])}
-        template_manager.CopyTemplate(controllerFileName, "console_controller.py", keywords, TemplatesRoot)
+        template_manager.CopyTemplate(controllerFileName, "pygame_controller.py", keywords, TemplatesRoot)
     
     def help(self):
-        """  """
+        """ Print Command usage """
         print "Usage: pbf {category} {command} [path/to/controller]".format(category=self.category, command=self.command)
-        print "\tWill create a Console Controller at the path given"
+        print "\tWill create a Pygame Controller at the path given"
     
-command_manager.RegisterCommand(NewConsoleController)
+command_manager.RegisterCommand(NewPygameController)
---
+++ pbf/kao_gui/templates/pygame_controller.py
@@ -0,0 +1,10 @@
+%ViewImport%
+from kao_gui.pygame.pygame_controller import PygameController
+
+class %ControllerName%(PygameController):
+    """ Controller for a *** """
+    
+   def __init__(self):
+        """ Initialize the *** Controller """
+        screen = %ViewName%()
+        PygameController.__init__(self, screen)
