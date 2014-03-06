7e69b91df0c9da9d1591be8d556739a7a7e034be
Chris Loew <cloew123@gmail.com>
2014-03-05 17:13:36 -0700

    Fixed Pygame Controller template

--- pbf/kao_gui/templates/pygame_controller.py
+++ pbf/kao_gui/templates/pygame_controller.py
@@ -4,7 +4,7 @@ from kao_gui.pygame.pygame_controller import PygameController
 class %ControllerName%(PygameController):
     """ Controller for a *** """
     
-   def __init__(self):
+    def __init__(self):
         """ Initialize the *** Controller """
         screen = %ViewName%()
         PygameController.__init__(self, screen)
